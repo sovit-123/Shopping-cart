5c27a2f767596c8871b2dc663f74fa52f79e25e5
sovit-123 <sovitrath5@gmail.com>
2018-10-26 19:25:49 +0530

    Updates

---
+++ precache-manifest.b024d286aa34485d9b0d40151b41b821.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/4.108dbfda.jpg"
+  },
+  {
+    "revision": "3863078d325453c2daf0",
+    "url": "/Shopping-cart/static/css/main.25061002.chunk.css"
+  },
+  {
+    "revision": "46e04b11e479a72b6f8e",
+    "url": "/Shopping-cart/static/js/1.46e04b11.chunk.js"
+  },
+  {
+    "revision": "e8abc0c1a4f8184a8317",
+    "url": "/Shopping-cart/static/js/runtime~main.e8abc0c1.js"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/1.108dbfda.jpg"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/2.108dbfda.jpg"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/3.108dbfda.jpg"
+  },
+  {
+    "revision": "3863078d325453c2daf0",
+    "url": "/Shopping-cart/static/js/main.3863078d.chunk.js"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/5.108dbfda.jpg"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/6.108dbfda.jpg"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/7.108dbfda.jpg"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/8.108dbfda.jpg"
+  },
+  {
+    "revision": "108dbfdab9e468d014f13cac320e9ab3",
+    "url": "/Shopping-cart/static/media/9.108dbfda.jpg"
+  },
+  {
+    "revision": "3f6052e3105cc9e7d58631d76c7e8361",
+    "url": "/Shopping-cart/index.html"
+  }
+];
